0942f5e171824abfaddebacaf649f78c7f714b89
Lauren Beatty <lauren.beatty@treasure-data.com>
2021-04-03 18:00:36 -0500

    write list of TODOs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// TODO
+// 1. Connect to firestore somehow
+// 2. put something in the App so I can update firestore
+// 3. Display that stuff
+
 function App() {
   return (
     <div className="App">
