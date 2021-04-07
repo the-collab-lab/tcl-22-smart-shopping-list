f3658345961c92be5c795078927794acc99969f5
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2021-04-07 14:11:20 -0400

    remove components that aren't used

--- src/components/AddItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AddItem = () => {
-  return <h2>Add Item!</h2>;
-};
-
-export default AddItem;
--- src/components/List.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const List = () => {
-  return <h2>LIST!</h2>;
-};
-
-export default List;
