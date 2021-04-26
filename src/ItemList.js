370346b75a1eaf07accac5b0cde6c8448ff8e0b9
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2021-04-26 19:51:29 -0400

    conditionally render a welcome prompt when list is empty

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function ItemList(props) {
+  let history = useHistory();
+  const redirect = () => {
+    history.push('/additems');
+  };
+
   return (
     <div>
       {props.loading && <span>Collection: Loading...</span>}
       {props.error && !props.loading && <strong>Error: {props.error}</strong>}
+      {props.list && props.list.length === 0 && (
+        <>
+          <p>Your shopping list is currently empty.</p>
+          <button onClick={redirect}>Add Item</button>
+        </>
+      )}
       {props.list && props.list.length > 0 && (
         <>
           <h2>Shopping List:</h2>
