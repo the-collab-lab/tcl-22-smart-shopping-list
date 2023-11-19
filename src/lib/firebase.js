8a42eb34704b69ff1ef66b12516d98711469b678
itsoliviasparks <itsoliviasparks@gmail.com>
2023-11-18 20:37:02 -0700

    adding new database & hosting

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: 'AIzaSyA0UVyQeViBUArtY0kfWxkD3TzhoSnVfcw',
-  authDomain: 'tcl-22-shopping-list.firebaseapp.com',
-  projectId: 'tcl-22-shopping-list',
-  storageBucket: 'tcl-22-shopping-list.appspot.com',
-  messagingSenderId: '908757844017',
-  appId: '1:908757844017:web:27af0d8875560207361510',
+  apiKey: 'AIzaSyAa8sRqomXUfeBDAKB9t3Dnq9uQup9E5cE',
+  authDomain: 'tcl-22-smart-shopping-list.firebaseapp.com',
+  projectId: 'tcl-22-smart-shopping-list',
+  storageBucket: 'tcl-22-smart-shopping-list.appspot.com',
+  messagingSenderId: '116137204814',
+  appId: '1:116137204814:web:e090827ae2837258b0371f',
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
